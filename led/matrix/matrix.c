ab96953c8af2a3c26081f829985d173089f8eb6a
root <kanekotakashi2009@gmail.com>
2014-03-16 03:01:55 +0900

    char A

--- led/matrix/matrix.c
+++ led/matrix/matrix.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <bcm2835.h>
-int main()
-{
-    int i,x,y;
-    int pinX[5]={
+
+int pinX[5]={
             RPI_GPIO_P1_26,
             RPI_GPIO_P1_24,
             RPI_GPIO_P1_22,
             RPI_GPIO_P1_18,
             RPI_GPIO_P1_16};
-    int pinY[5]={
+int pinY[5]={
             RPI_GPIO_P1_07,
             RPI_GPIO_P1_11,
             RPI_GPIO_P1_12,
             RPI_GPIO_P1_15,
             RPI_GPIO_P1_19};
-    int high_low[2]={LOW,HIGH};
-
+void high(int x,int y) {
+    int i;
+    for (i=0;i<5;i++) {
+        bcm2835_gpio_write(pinY[i], HIGH);
+        bcm2835_gpio_write(pinX[i], LOW);
+    }
+    bcm2835_gpio_write(pinY[y], LOW);
+    bcm2835_gpio_write(pinX[x], HIGH);
+    bcm2835_delay(1);
+}
+int main()
+{
+    int i,x,y;
     if (!bcm2835_init()) {
         return 1;
     }
@@ -31,28 +40,24 @@ int main()
         }
     }
 
-    i=0;
     while (1)
     {
 
-        for(x=0;x<5;x++) {
-            for(y=0;y<5;y++) {
-                if (y==i) {
-               	    bcm2835_gpio_write(pinY[y], LOW);
-                } else {
-               	    bcm2835_gpio_write(pinY[y], HIGH);
-                }
-                if (x==i) {
-               	    bcm2835_gpio_write(pinX[x], HIGH);
-                } else {
-               	    bcm2835_gpio_write(pinX[x], LOW);
-                }
-            }
+        for (i=0;i<50;i++) {
+            //"A"
+            high(2,0);
+            high(1,1);
+            high(3,1);
+            high(0,2);
+            high(4,2);
+            high(0,3);
+            high(1,3);
+            high(2,3);
+            high(3,3);
+            high(4,3);
+            high(0,4);
+            high(4,4);
         }
-        // wait a bit
-        bcm2835_delay(5);
-        i++;
-        if (i == 5) i=0;
 
     }
     bcm2835_close();
